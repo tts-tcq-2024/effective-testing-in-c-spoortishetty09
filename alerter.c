53e3e73fd71e76da6278453fc4ff1753046e6041
spoortishetty09 <spoortishetty45@gmail.com>
2024-08-21 09:54:51 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,44 +1,25 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
+#include "printMessage.h"
+#include "alerter.h"
 
-int alertFailureCount = 0;
 
-int networkAlertStub(float celcius) {
-    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-    if (celcius > 200) { // Simulating a failure for high temperatures
-        return 500;
-    }
+int networkAlertMock(float celcius) {
+    printMessage(celcius);
+    // Return 200 for ok
+    // Return 500 for not-ok
+    // stub always succeeds and returns 200
     return 200;
 }
 
-void alertInCelcius(float farenheit) {
-    float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlertStub(celcius);
-    if (returnCode != 200) {
-        // This should increment failure count, but there's a bug
-        alertFailureCount += 0; // Bug: should be 'alertFailureCount += 1;'
-    }
-}
 
-void test_alertInCelcius() {
-    alertFailureCount = 0;
-
-    alertInCelcius(400.5);
-    assert(alertFailureCount == 1); // Expected to fail due to the bug
-
-    alertInCelcius(303.6);
-    assert(alertFailureCount == 2); // Expected to fail due to the bug
-
-    // New test case to explicitly check for the bug
-    if (alertFailureCount != 2) {
-        printf("Bug detected: alertFailureCount is %d, expected 2.\n", alertFailureCount);
-    } else {
-        printf("No bug detected: alertFailureCount is %d.\n", alertFailureCount);
-    }
-}
 
 int main() {
-    test_alertInCelcius();
+    alertInCelcius(400.5,networkAlertMock);
+    alertInCelcius(303.6,networkAlertMock);
+    assert(alertFailureCount==1);
+    assert(alertFailureCount==0);
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
