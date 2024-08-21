b9413497f6dce06ed11074c6454c72c4b573649c
spoortishetty09 <spoortishetty45@gmail.com>
2024-08-21 10:03:18 +0530

    Update alerter.h

--- alerter.h
+++ alerter.h
@@ -22,8 +22,3 @@ void alertInCelcius(float farenheit,int (*networkAlert)(float)) {
     }
 }
 
-void printMsg(float temperature) {
-    printf("ALERT: Temperature is %.1f celcius.\n", temperature);
-}
-
-void (*printMessage)(float message) = printMsg;
