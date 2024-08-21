0c0f6aee7c96027f9267f68d71687d47e72b7869
spoortishetty09 <spoortishetty45@gmail.com>
2024-08-21 10:10:33 +0530

    Update alerter.h

--- alerter.h
+++ alerter.h
@@ -3,7 +3,7 @@
 int alertFailureCount = 0;
 
 int networkAlerter(float celcius) {
-    printMessage(celcius);
+    printMsg(celcius);
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
